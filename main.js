75f2b6069fd3c30929e6fad560e69f870c595c51
Victor Lopes <90704862+vlopinhos@users.noreply.github.com>
2022-05-31 18:47:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("#formulario");
+
+let strDados = localStorage.getItem("db");
+let objDados = {};
+
+if(strDados) {
+    objDados = JSON.parse(strDados);
+}else {
+    objDados = {
+        usuarios: [{
+            email: "teste@email.com",
+            user: "teste",
+            password: "123"
+        },
+        {
+            email: "adm@email.com",
+            user: "adm",
+            password: "124"
+        }]
+    };
+}
+
+form.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    const strEmail = document.querySelector('#email').value;//pegar do documento
+    const strUser = document.querySelector('#user').value;
+    const strPassword = document.querySelector('#password').value;
+
+    let novoUsuario = {
+        email: strEmail,
+        user: strUser,
+        password: strPassword
+    };
+
+    objDados.usuarios.push(novoUsuario);
+    
+    localStorage.setItem("db", JSON.stringify(objDados));
+});
